e1043ca159e5223deaedd024057bc74d5a7a7a66
Shuai_Huang <shuaihuang.sjtu@gmail.com>
2015-08-03 23:00:14 +0800

    Verify the algorithm. Code reconstruction is needed.

---
+++ Codes/FacePP/FaceMorphing/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using FaceppSDK;
+
+namespace FaceMorphing
+{
+    class FaceMorph
+    {
+        enum LandMark { LeftEye, RightEye, Nose, LeftMouth, RightMouth };
+        private Image<Bgr, byte> srcFaceImg, dstFaceImg, intermediateFaceImg;
+        private string srcPath, dstPath;
+        private Size imgSize;
+        private string apiKey, apiSecret;
+        private IList<System.Drawing.Point> srcLandmark, dstLandmark, itermediateLandmark;
+
+        FaceService faceService;
+
+        public FaceMorph()
+        {
+            apiKey = "847e6315f892e21449da5f4077c5104f";
+            apiSecret = "BmskojfFyrZVQhkLfNSnRzX-lK8musO6";
+            faceService = new FaceService(apiKey, apiSecret);
+
+            srcLandmark = new List<System.Drawing.Point>();
+            dstLandmark = new List<System.Drawing.Point>();
+            itermediateLandmark = new List<System.Drawing.Point>();
+        }
+
+        public void setImgSize(int _imgWidth, int _imgHeight)
+        {
+            imgSize.Height = _imgHeight;
+            imgSize.Width = _imgWidth;
+        }
+
+        public void setSrcFaceImg(string _filePath)
+        {
+            Image<Bgr, byte> inputImg = new Image<Bgr, byte>(_filePath);
+            inputImg.Resize(imgSize.Width, imgSize.Height, Inter.Linear);
+            srcFaceImg = inputImg.Clone();
+            srcPath = _filePath;
+        }
+
+        public void setDstFaceImg(string _filePath)
+        {
+            Image<Bgr, byte> inputImg = new Image<Bgr, byte>(_filePath);
+            inputImg.Resize(imgSize.Width, imgSize.Height, Inter.Linear);
+            dstFaceImg = inputImg.Clone();
+            dstPath = _filePath;
+        }
+
+        public void setItermediateFaceImg()
+        {
+            Image<Bgr, byte> inputImg = new Image<Bgr, byte>(imgSize);
+            inputImg.SetZero();
+            intermediateFaceImg = inputImg.Clone();
+        }
+
+        public void getLandmarks()
+        {
+            StreamReader srcReader = new StreamReader("D:\\Codes\\datasets\\face_morph\\result_bbt_face_1.txt");
+            for(int cnt = 0; cnt < 5; cnt++)
+            {
+                System.Drawing.Point tempPoint = new System.Drawing.Point();
+                tempPoint.X = (int)((Convert.ToSingle(srcReader.ReadLine()) + 0.5) * (imgSize.Width));
+                tempPoint.Y = (int)((Convert.ToSingle(srcReader.ReadLine()) + 0.5) * (imgSize.Height));
+                srcLandmark.Add(tempPoint);
+            }
+
+            StreamReader dstReader = new StreamReader("D:\\Codes\\datasets\\face_morph\\result_bbt_face_2.txt");
+            for (int cnt = 0; cnt < 5; cnt++)
+            {
+                System.Drawing.Point tempPoint = new System.Drawing.Point();
+                tempPoint.X = (int)((Convert.ToSingle(dstReader.ReadLine()) + 0.5) * (imgSize.Width));
+                tempPoint.Y = (int)((Convert.ToSingle(dstReader.ReadLine()) + 0.5) * (imgSize.Height));
+                dstLandmark.Add(tempPoint);
+            }
+
+            for(int cnt = 0; cnt < 5; cnt++)
+            {
+                System.Drawing.Point tempPoint = new System.Drawing.Point();
+                tempPoint.X = (srcLandmark[cnt].X / 2 + dstLandmark[cnt].X / 2);
+                tempPoint.Y = (srcLandmark[cnt].Y / 2 + dstLandmark[cnt].Y / 2);
+                itermediateLandmark.Add(tempPoint);
+            }
+        }
+
+        public void fuseRegion()
+        {
+            PointF[] srcSet = new PointF[] {srcLandmark[0], 
+                srcLandmark[1],
+                srcLandmark[2]
+            };
+
+            PointF[] dstSet = new PointF[] {dstLandmark[0], 
+                dstLandmark[1],
+                dstLandmark[2]
+            };
+
+            PointF[] itermediateSet = new PointF[] {itermediateLandmark[0], 
+                itermediateLandmark[1],
+                itermediateLandmark[2]
+            };
+
+            Mat srcRotMat = new Mat();
+            Mat dstRotMat = new Mat();
+
+            srcRotMat = CvInvoke.GetAffineTransform(srcSet, itermediateSet);
+            dstRotMat = CvInvoke.GetAffineTransform(dstSet, itermediateSet);
+
+            Image<Bgr, byte> srcWarp = new Image<Bgr, byte>(imgSize);
+            srcWarp.SetZero();
+            Image<Bgr, byte> dstWarp = new Image<Bgr, byte>(imgSize);
+            dstWarp.SetZero();
+
+            CvInvoke.WarpAffine(srcFaceImg, srcWarp, srcRotMat, imgSize);
+            CvInvoke.WarpAffine(dstFaceImg, dstWarp, dstRotMat, imgSize);
+
+            intermediateFaceImg = srcWarp / 2 + dstWarp / 2;
+
+            srcWarp.Save("D:\\Codes\\datasets\\face_morph\\result_bbt_face_warp_1.jpg");
+            dstWarp.Save("D:\\Codes\\datasets\\face_morph\\result_bbt_face_warp_2.jpg");
+            intermediateFaceImg.Save("D:\\Codes\\datasets\\face_morph\\result_bbt_face_warp.jpg");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FaceMorph faceMorph = new FaceMorph();
+            faceMorph.setImgSize(140, 140);
+            faceMorph.setSrcFaceImg("D:\\Codes\\datasets\\face_morph\\result_bbt_face_1.jpg");
+            faceMorph.setDstFaceImg("D:\\Codes\\datasets\\face_morph\\result_bbt_face_2.jpg");
+            faceMorph.setItermediateFaceImg();
+            faceMorph.getLandmarks();
+            faceMorph.fuseRegion();
+        }
+    }
+}
--- Codes/FacePP/FacePP/Program.cs
+++ Codes/FacePP/FacePP/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.IO;
+
 using FaceppSDK;
 
 using Emgu.CV;
@@ -15,51 +17,88 @@ namespace FacePP
 {
     class Program
     {
+        enum FeaturePoint { LeftEye, RightEye, Nose, LeftMouth, RightMouth };
+
         static void Main(string[] args)
         {
             String apiKey = "847e6315f892e21449da5f4077c5104f";
             String apiSecret = "BmskojfFyrZVQhkLfNSnRzX-lK8musO6";
             FaceService faceService = new FaceService(apiKey, apiSecret);
 
-            string filePath = "G:\\data\\Big-Bang-Theory-6.jpg";
+            string filePath = "D:\\Codes\\datasets\\face_morph\\bbt.jpg";
             DetectResult detectResult = faceService.Detection_DetectImg(filePath);
 
             Image<Bgr, Byte> srcImg = new Image<Bgr, Byte>(filePath);
-
+            
             for(int cnt=0; cnt < detectResult.face.Count; cnt++)
             {
-                FaceppSDK.Point center  = detectResult.face[cnt].position.center;
-                FaceppSDK.Point leftEye = detectResult.face[cnt].position.eye_left;
-                FaceppSDK.Point rightEye = detectResult.face[cnt].position.eye_right;
-                FaceppSDK.Point leftMouth = detectResult.face[cnt].position.mouth_left;
-                FaceppSDK.Point rightMouth = detectResult.face[cnt].position.mouth_right;
-                FaceppSDK.Point nose = detectResult.face[cnt].position.nose;
-                FaceppSDK.Age age = detectResult.face[cnt].attribute.age;
-                FaceppSDK.Gender gender = detectResult.face[cnt].attribute.gender;
-                FaceppSDK.Race race = detectResult.face[cnt].attribute.race;
+                string pointFileName = String.Format("D:\\Codes\\datasets\\face_morph\\result_bbt_face_{0}.txt", cnt);
+                FileStream fileStream = new FileStream(pointFileName, FileMode.Create);
+                StreamWriter streamWriter = new StreamWriter(fileStream);
 
-                Console.WriteLine("Center: " + center.x.ToString() + ", " + center.y.ToString());
-                Console.WriteLine("Left Eye: " + leftEye.x.ToString() + ", " + leftEye.y.ToString());
-                Console.WriteLine("Right Eye: " + rightEye.x.ToString() + ", " + rightEye.y.ToString());
-                Console.WriteLine("Left Mouth: " + leftMouth.x.ToString() + ", " + leftMouth.y.ToString());
-                Console.WriteLine("Right Mouth: " + rightMouth.x.ToString() + ", " + rightMouth.y.ToString());
-                Console.WriteLine("Nose: " + nose.x.ToString() + ", " + nose.y.ToString());
-                Console.WriteLine("Age: " + age.value.ToString());
-                Console.WriteLine("Gender: " + gender.value.ToString());
-                Console.WriteLine("Race: " + race.value.ToString());
-
-                Rectangle faceRect = new Rectangle((int)((center.x - 0.5 * detectResult.face[cnt].position.width) * detectResult.img_width / 100),
-                    (int)((center.y - 0.5 * detectResult.face[cnt].position.height) * detectResult.img_height / 100),
-                    (int)((detectResult.face[cnt].position.height) * detectResult.img_height / 100), 
-                    (int)((detectResult.face[cnt].position.width) * detectResult.img_width / 100));
-                srcImg.Draw(faceRect, new Bgr(0, 255, 0), 3);
-                Cross2DF leftEyeF = new Cross2DF(new System.Drawing.Point((int)(nose.x * detectResult.img_width / 100), (int)(nose.y * detectResult.img_height / 100)), 5, 5);
-                srcImg.Draw(leftEyeF, new Bgr(255, 0, 0), 3);
-
+                Rectangle faceRect = new Rectangle(
+                    (int)(detectResult.face[cnt].position.center.x * srcImg.Width /100 - detectResult.face[cnt].position.width * srcImg.Width * 0.5 / 100),
+                    (int)(detectResult.face[cnt].position.center.y * srcImg.Height / 100 - detectResult.face[cnt].position.height * srcImg.Height * 0.5 / 100),
+                    (int)detectResult.face[cnt].position.width * srcImg.Width / 100,
+                    (int)detectResult.face[cnt].position.height * srcImg.Height / 100);
+                
                 Image<Bgr, byte> faceImg = srcImg.GetSubRect(faceRect);
-                faceImg.Save(String.Format("G:\\data\\Big-Bang-Theory-6-face-{0}.jpg", cnt));
 
-                srcImg.Save("G:\\data\\Big-Bang-Theory-6-result.jpg");
+                string fileName = String.Format("D:\\Codes\\datasets\\face_morph\\result_bbt_face_{0}.jpg", cnt);
+                faceImg.Save(fileName);
+
+                IList<FaceppSDK.Point> featurePoints = new List<FaceppSDK.Point>();
+                //featurePoints.Add(detectResult.face[cnt].position.center);
+                FaceppSDK.Point tempPoint1 = new FaceppSDK.Point();
+                tempPoint1.x = (detectResult.face[cnt].position.eye_left.x
+                    - detectResult.face[cnt].position.center.x) / detectResult.face[cnt].position.width;
+                tempPoint1.y = (detectResult.face[cnt].position.eye_left.y
+                    - detectResult.face[cnt].position.center.y) / detectResult.face[cnt].position.height;
+                featurePoints.Add(tempPoint1);
+
+                FaceppSDK.Point tempPoint2 = new FaceppSDK.Point();
+                tempPoint2.x = (detectResult.face[cnt].position.eye_right.x
+                    - detectResult.face[cnt].position.center.x) / detectResult.face[cnt].position.width;
+                tempPoint2.y = (detectResult.face[cnt].position.eye_right.y
+                    - detectResult.face[cnt].position.center.y) / detectResult.face[cnt].position.height;
+                featurePoints.Add(tempPoint2);
+
+                FaceppSDK.Point tempPoint3 = new FaceppSDK.Point();
+                tempPoint3.x = (detectResult.face[cnt].position.mouth_left.x
+                    - detectResult.face[cnt].position.center.x) / detectResult.face[cnt].position.width;
+                tempPoint3.y = (detectResult.face[cnt].position.mouth_left.y
+                    - detectResult.face[cnt].position.center.y) / detectResult.face[cnt].position.height;
+                featurePoints.Add(tempPoint3);
+
+                FaceppSDK.Point tempPoint4 = new FaceppSDK.Point();
+                tempPoint4.x = (detectResult.face[cnt].position.mouth_right.x
+                    - detectResult.face[cnt].position.center.x) / detectResult.face[cnt].position.width;
+                tempPoint4.y = (detectResult.face[cnt].position.mouth_right.y
+                    - detectResult.face[cnt].position.center.y) / detectResult.face[cnt].position.height;
+                featurePoints.Add(tempPoint4);
+
+                FaceppSDK.Point tempPoint5 = new FaceppSDK.Point();
+                tempPoint5.x = (detectResult.face[cnt].position.nose.x
+                    - detectResult.face[cnt].position.center.x) / detectResult.face[cnt].position.width;
+                tempPoint5.y = (detectResult.face[cnt].position.nose.y
+                    - detectResult.face[cnt].position.center.y) / detectResult.face[cnt].position.height;
+                featurePoints.Add(tempPoint5);
+
+                foreach (FaceppSDK.Point featurePoint in featurePoints)
+                {
+                    streamWriter.WriteLine(featurePoint.x.ToString());
+                    streamWriter.WriteLine(featurePoint.y.ToString());
+
+                    System.Drawing.PointF point = new System.Drawing.PointF((float)featurePoint.x * srcImg.Width / 100,
+                        (float)featurePoint.y * srcImg.Height / 100);
+                    Cross2DF cross = new Cross2DF(point, (float)3.0, (float)3.0);
+                    srcImg.Draw(cross, new Bgr(0, 255, 0), 3);
+                }
+
+                streamWriter.Flush();
+                streamWriter.Close();
+                fileStream.Close();
+                //srcImg.Save("D:\\Codes\\datasets\\face_morph\\result_bbt.jpg");
             }
         }
     }
